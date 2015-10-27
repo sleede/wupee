22da2e2c3530f9d327a55a8e2964324d3e5f81b8
Nicolas Florentin <nicolas@sleede.com>
2015-10-27 18:13:11 +0100

    fix pb of class name in factories

--- spec/factories/notification.rb
+++ spec/factories/notification.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
-  factory :notification do
+  factory :notification, class: NotifyWith::Notification do
     association :receiver, factory: :user, strategy: :create
     association :attached_object, factory: :message, strategy: :create
-    notification_type_id NotificationType.find_by_name('notify_new_message')
+    notification_type_id NotifyWith::NotificationType.find_by_name('notify_new_message')
   end
 end
