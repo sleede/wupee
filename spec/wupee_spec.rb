eca40b0141f8be7f9faf6ae9c68f9b21393d8a8e
Nicolas Florentin <nicolas@sleede.com>
2015-10-29 14:14:43 +0100

    improves wupee specs

--- spec/wupee_spec.rb
+++ spec/wupee_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe Wupee do
-  it "should can set a mailer" do
+  it "has a setter for mailer" do
     expect(Wupee).to respond_to(:mailer=).with(1).argument
   end
 
-  it "should can get a mailer" do
+  it "has a getter for mailer" do
     expect(Wupee).to respond_to(:mailer)
   end
 end
