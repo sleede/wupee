16f7cdf0d2d915f5bcda1dbf7d9d61057f68667e
Nicolas Florentin <nicolas@sleede.com>
2015-10-30 16:20:03 +0100

    try to improves vocabulary proposed in json description of the notification

--- lib/generators/wupee/notification/notification_generator.rb
+++ lib/generators/wupee/notification/notification_generator.rb
@@ -1,12 +1,4 @@
 class Wupee::NotificationGenerator < Rails::Generators::NamedBase
-#   def create_notification_mail_template_file
-#     create_file "app/views/notifications_mailer/#{file_name}.html.erb", <<-FILE
-# <%# this is a mail template of notifcation #{file_name} %>
-# <p><%= @recipient.name %></p>
-# <p><%= @attached_object.body %></p>
-#     FILE
-#   end
-
   def add_notification_subject
     inject_into_file "config/locales/#{I18n.locale.to_s}.yml", after: /email_subjects:\n/ do
 <<-CODE
@@ -17,9 +9,9 @@ CODE
 
   def create_notification_json_template_file
     create_file "app/views/wupee/api/notifications/_#{file_name}.json.jbuilder", <<-FILE
-json.title notification.notification_type.name
-json.description 'a notification description'
-json.url 'a url for redirect to attached_object'
+json.subject notification.notification_type.name
+json.body "body"
+json.url "url"
     FILE
   end
 
