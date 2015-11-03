d238bf70d582984cfd166506a8f5e05281e71a86
Nicolas Florentin <nicolas@sleede.com>
2015-11-03 14:28:53 +0100

    fix order of migrations and pb with foreign keys

--- db/migrate/20151029113101_create_wupee_notification_type_configurations.rb
+++ db/migrate/20151029113101_create_wupee_notification_type_configurations.rb
@@ -1,12 +1,13 @@
 class CreateWupeeNotificationTypeConfigurations < ActiveRecord::Migration
   def change
     create_table :wupee_notification_type_configurations do |t|
-      t.belongs_to :notification_type, foreign_key: true
+      t.belongs_to :notification_type
       t.belongs_to :receiver, polymorphic: true
       t.integer :value, default: 0
       t.timestamps null: false
     end
 
+    add_foreign_key :wupee_notification_type_configurations, :wupee_notification_types, column: :notification_type_id
     add_index :wupee_notification_type_configurations, [:notification_type_id], name: "idx_wupee_notif_type_config_on_notification_type_id"
     add_index :wupee_notification_type_configurations, [:receiver_type, :receiver_id], name: "idx_wupee_notif_typ_config_on_receiver_type_and_receiver_id"
   end
--- db/migrate/20151029113107_create_wupee_notifications.rb
+++ db/migrate/20151029113107_create_wupee_notifications.rb
@@ -3,12 +3,14 @@ class CreateWupeeNotifications < ActiveRecord::Migration
     create_table :wupee_notifications do |t|
       t.references :receiver, polymorphic: true
       t.references :attached_object, polymorphic: true
-      t.belongs_to :notification_type, foreign_key: true
+      t.belongs_to :notification_type
       t.integer :notification_type_id
       t.boolean :is_read, default: false
       t.boolean :is_sent, default: false
 
       t.timestamps null: false
     end
+
+    add_foreign_key :wupee_notifications, :wupee_notification_types, column: :notification_type_id
   end
 end
