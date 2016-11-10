cfb503679d7d6d2b084abf7d6b26b22cbb059b4b
Ramon Caldeira <ramon.caldeira@barbaruiva.com>
2016-11-10 13:23:59 -0200

    customize for lasa

--- app/models/concerns/wupee/receiver.rb
+++ app/models/concerns/wupee/receiver.rb
@@ -2,13 +2,19 @@ module Wupee
   module Receiver
     extend ActiveSupport::Concern
 
+    LASA_PROJECT_ID = 132
+
     included do
       has_many :notifications, as: :receiver, dependent: :destroy, class_name: "Wupee::Notification"
       has_many :notification_type_configurations, as: :receiver, dependent: :destroy, class_name: "Wupee::NotificationTypeConfiguration"
 
       after_create do
         Wupee::NotificationType.pluck(:id).each do |notification_type_id|
-          Wupee::NotificationTypeConfiguration.create!(notification_type_id: notification_type_id, receiver: self)
+          Wupee::NotificationTypeConfiguration.create!(
+            notification_type_id: notification_type_id, 
+            receiver: self,
+            value: (self.project_id == LASA_PROJECT_ID) ? 1 : 0
+          )
         end
       end
     end
--- app/models/wupee/notification_type.rb
+++ app/models/wupee/notification_type.rb
@@ -5,12 +5,18 @@ class Wupee::NotificationType < ActiveRecord::Base
   has_many :notification_type_configurations, foreign_key: :notification_type_id, dependent: :destroy
   has_many :notifications, foreign_key: :notification_type_id, dependent: :destroy
 
+  LASA_PROJECT_ID = 132
+
   def self.create_configurations_for(*receivers)
     class_eval do
-      receivers.each do |receiver|
+      receivers.each do |receiver_type|
         after_create do
-          receiver.to_s.constantize.pluck(:id).each do |receiver_id|
-            Wupee::NotificationTypeConfiguration.create!(notification_type: self, receiver_type: receiver, receiver_id: receiver_id)
+          receiver_type.to_s.constantize.find_each do |receiver|
+            Wupee::NotificationTypeConfiguration.create!(
+              notification_type: self, 
+              receiver: receiver,
+              value: (receiver.project_id == LASA_PROJECT_ID) ? 1 : 0
+            )
           end
         end
       end
